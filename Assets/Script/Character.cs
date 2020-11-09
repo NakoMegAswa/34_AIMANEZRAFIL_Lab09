3f267e5c5141b47a66e8f42f08afad2f1de61aae
NakoMegAswa <72852456+NakoMegAswa@users.noreply.github.com>
2020-11-10 01:09:29 +0800

    Add files via upload

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float speed;
+    public float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += new Vector3(0, 0, Time.deltaTime * speed);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, Time.deltaTime * -speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * rotateSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * -rotateSpeed, 0));
+        }
+    }
+
+}
---
+++ Assets/Script/TestScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject positionText;
+    public float speed;
+    public Text countText;
+    
+    public Text movementText;
+
+    private Rigidbody rb;
+    private int count;
+    private int Movement;
+
+    public Vector3 pos;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionText.GetComponent<Text>().text = "Position: " + pos;
+
+        pos = transform.position;
+        Debug.Log("Position is " + pos);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     positionText.GetComponent<Text>().text = " " + count.ToString();
+     
+    }
+}
