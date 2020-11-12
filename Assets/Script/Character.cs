42f89488ebd05f8d29c9e95931ffd3bef6a49314
NakoMegAswa <72852456+NakoMegAswa@users.noreply.github.com>
2020-11-12 21:12:08 +0800

    Add files via upload

--- Assets/Script/Character.cs
+++ Assets/Script/Character.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Character : MonoBehaviour
 {
+    public GameObject Location;
     public float speed;
     public float rotateSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Location.GetComponent<Text>().text = "Start Function";
+
+     
     }
 
     // Update is called once per frame
     void Update()
     {
+        Location.GetComponent<Text>().text = ("Position: " + (GameObject.FindGameObjectWithTag("Player").transform.position));
+
         if (Input.GetKey(KeyCode.W))
         {
             //transform.position += new Vector3(0, 0, Time.deltaTime * speed);
